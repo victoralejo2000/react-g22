7d3efea09ce314081accf54d685bb477e52b660f
victoralejo2000 <victor_alejo2000@hotmail.com>
2023-08-15 23:31:17 -0500

    new file

---
+++ src/Footer.jsx
@@ -0,0 +1,14 @@
+
+
+const Footer=()=>{
+    return (
+        <div>
+            <h2>Practica</h2>
+            <div>
+                <h2>Hola Mundo</h2>
+            </div>
+        </div>
+    )
+};
+
+export default Footer;
---
+++ src/Test.jsx
@@ -0,0 +1,11 @@
+
+
+const Test=()=>{
+    return (
+        <div>
+            <h1>Somos el G22</h1>
+        </div>
+    )
+};
+
+export default Test;
